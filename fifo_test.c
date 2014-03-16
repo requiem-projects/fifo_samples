a15ba3c89a5df79ffba8e0aff72359f366cddcf5
mesobius <jeanf.garcia71@gmail.com>
2014-03-16 10:43:39 +0100

    Create fifo_test.c

---
+++ fifo_test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+        int fifo, c=0;
+        char buf[200];
+
+        char fifo_name[] = "/var/log/proftpd_log_all.fifo";
+        fifo= open(fifo_name, O_RDONLY);
+
+        // receive messages
+        while (1)
+        {
+            if (read(fifo, &buf, sizeof(char)*200) > 0)
+            {
+                printf("%s \n", buf);
+                c=0;
+            }
+            usleep(100000);
+            c++;
+            if (c>20)
+                break;
+        }
+
+        printf("exit");
+        close(fifo);
+
+    return 0;
+}
